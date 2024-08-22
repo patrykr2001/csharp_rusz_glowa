3193a203c33c6cedd749cf8720d77c916f7df75a
Patryk Rakowski <patrykr2001@gmail.com>
2024-08-22 16:06:51 +0200

    Finał eksperymenty z kontrolkami

--- ExperimentWithControls/ExperimentWithControls/MainWindow.xaml.cs
+++ ExperimentWithControls/ExperimentWithControls/MainWindow.xaml.cs
@@ -25,5 +25,60 @@ namespace ExperimentWithControls
         {
             number.Text = numberTextBlock.Text;
         }
+
+        private void numberTextBlock_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !int.TryParse(e.Text, out int result);
+        }
+
+        private void smallSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            number.Text = smallSlider.Value.ToString();
+        }
+
+        private void bigSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            number.Text = bigSlider.Value.ToString("000-000-000");
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            if(sender is RadioButton radioButton)
+            {
+                number.Text = radioButton.Content.ToString();
+            }
+        }
+
+        private void myListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(myListBox.SelectedItem is ListBoxItem listBoxItem)
+            {
+                number.Text = listBoxItem.Content.ToString();
+            }
+        }
+
+        private void editableCombiBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(editableCombiBox.SelectedItem is ListBoxItem listBoxItem)
+            {
+                number.Text = listBoxItem.Content.ToString();
+            }
+        }
+
+        private void readOnlyComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if(readOnlyComboBox.SelectedItem is ListBoxItem listBoxItem)
+            {
+                number.Text = listBoxItem.Content.ToString();
+            }
+        }
+
+        private void editableCombiBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if(sender is ComboBox comboBox)
+            {
+                number.Text = comboBox.Text;
+            }
+        }
     }
 }
