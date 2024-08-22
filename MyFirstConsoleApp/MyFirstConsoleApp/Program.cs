308303c5f3098998d9f8ecfed571554ba4ae5f34
Patryk <patryk.rakowski@cwmlegal.pl>
2024-08-22 14:27:40 +0200

    Kolejny rozdział

--- MyFirstConsoleApp/MyFirstConsoleApp/Program.cs
+++ MyFirstConsoleApp/MyFirstConsoleApp/Program.cs
@@ -4,6 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        OperatorExamples();
+    }
+
+    private static void OperatorExamples()
+    {
+        int width = 3;
+
+        width++;
+
+        int height = 2 + 4;
+        int area = width * height;
+        Console.WriteLine(area);
+        
+        while (area < 50)
+        {
+            height++;
+            area = width * height;
+        }
+
+        do
+        {
+            width--;
+            area = width * height;
+        } while (area > 25);
+
+        string result = "Powierzchnia";
+        result = result + " wynosi " + area;
+        Console.WriteLine(result);
+
+        bool thruthValue = true;
+        Console.WriteLine(thruthValue);
     }
 }
