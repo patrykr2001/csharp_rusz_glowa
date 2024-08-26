9e52cb770b02da04747747e6430ae4061fbf926c
Patryk <patryk.rakowski@cwmlegal.pl>
2024-08-26 17:36:58 +0200

    Rzutowanie innital commit

---
+++ Rzutowanie/Rzutowanie/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Rzutowanie;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        float myFloatValue = 10;
+        int myIntValue = (int) myFloatValue;
+        Console.WriteLine("myIntValue równa się " + myIntValue);
+    }
+}
