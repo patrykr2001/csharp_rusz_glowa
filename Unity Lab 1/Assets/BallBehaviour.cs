f26f928cedd490740f64f9b815c6377f91045750
Patryk <patryk.rakowski@cwmlegal.pl>
2024-08-29 00:00:59 +0200

    Unity Lab 1 kolejne kroki

--- Unity Lab 1/Assets/BallBehaviour.cs
+++ Unity Lab 1/Assets/BallBehaviour.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class BallBehaviour : MonoBehaviour
 {
+    public float XRotation;
+    public float YRotation;
+    public float ZRotation;
+    public float DegreesPerSecond = 180.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,8 @@ public class BallBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 axis = new Vector3(XRotation, YRotation, ZRotation);
+        transform.RotateAround(Vector3.zero, axis, Time.deltaTime * DegreesPerSecond);
+        Debug.DrawRay(Vector3.zero, axis, Color.yellow, .5f);
     }
 }
