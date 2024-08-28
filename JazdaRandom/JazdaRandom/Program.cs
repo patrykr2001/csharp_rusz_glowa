c7b234c69e8f515fa2b28457e9cf69fbe6e12ec1
Patryk <patryk.rakowski@cwmlegal.pl>
2024-08-28 19:47:46 +0200

    Jazda Random

---
+++ JazdaRandom/JazdaRandom/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace JazdaRandom;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random random = new Random();
+        int randomInt = random.Next();
+        Console.WriteLine($"randomInt: {randomInt}");
+        
+        int zeroToNine = random.Next(10);
+        Console.WriteLine($"zeroToNine: {zeroToNine}");
+        
+        int dieRoll = random.Next(1, 7);
+        Console.WriteLine($"dieRoll : {dieRoll}");
+        
+        double randomDouble = random.NextDouble();
+        Console.WriteLine($"randomDouble: {randomDouble * 100}");
+        Console.WriteLine($"randomFloat: {(float)randomDouble * 100F}");
+        Console.WriteLine($"randomDecimal: {(decimal)randomDouble * 100M}");
+        
+        int zerOrOne = random.Next(2);
+        bool coinFlip = Convert.ToBoolean(zerOrOne);
+        Console.WriteLine($"coinFlip: {coinFlip}");
+    }
+}
