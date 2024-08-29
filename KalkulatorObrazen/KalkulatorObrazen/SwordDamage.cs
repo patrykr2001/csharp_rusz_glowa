5663c476cb5633518d51c22d80a56d34a08a8c0b
Patryk Rakowski <patrykr2001@gmail.com>
2024-08-29 12:44:23 +0200

    KalkulatorObrażeń

---
+++ KalkulatorObrazen/KalkulatorObrazen/SwordDamage.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KalkulatorObrazen
+{
+    internal class SwordDamage
+    {
+        public const int BASE_DAMAGE = 3;
+        public const int FLAME_DAMAGE = 2;
+
+        public int Roll;
+        private decimal MagicMultiplier = 1M;
+        private int FlamingDamage = 0;
+        public int Damage;
+
+        public void CalculateDamage()
+        {
+            Damage = (int)(Roll * MagicMultiplier) + BASE_DAMAGE + FLAME_DAMAGE;
+        }
+
+        public void SetMagic(bool isMagic)
+        {
+            if (isMagic)
+            {
+                MagicMultiplier = 1.75M;
+            }
+            else
+            {
+                MagicMultiplier = 1M;
+            }
+            CalculateDamage();
+        }
+
+        public void SetFlaming(bool isFlaming)
+        {
+            CalculateDamage();
+            if(isFlaming)
+            {
+                Damage += FLAME_DAMAGE;
+            }
+        }
+    }
+}
