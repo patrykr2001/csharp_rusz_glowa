74135c1c30093beda725145a1d15ed94e540c01f
Patryk <patryk.rakowski@cwmlegal.pl>
2024-10-03 18:14:10 +0200

    Replace "Hello, World!" with animal noise and behavior logic.

--- AnimalTypes/AnimalTypes/Program.cs
+++ AnimalTypes/AnimalTypes/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Animal[] animals =
+        [
+            new Wolf(false),
+            new Hippo(),
+            new Wolf(true),
+            new Wolf(false),
+            new Hippo()
+        ];
+
+        foreach (var animal in animals)
+        {
+            animal.MakeNoise();
+            if (animal is Hippo hippo)
+            {
+                hippo.Swim();
+            }
+
+            if (animal is Wolf wolf)
+            {
+                wolf.HuntInPack();
+            }
+            
+            Console.WriteLine();
+        }
     }
 }
