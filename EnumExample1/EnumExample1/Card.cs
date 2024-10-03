e2f8718f82ace5faa628f341e553eddc9f1223ac
Patryk <patryk.rakowski@cwmlegal.pl>
2024-10-03 20:56:04 +0200

    Add Name property to Card class for formatted card representation.

--- EnumExample1/EnumExample1/Card.cs
+++ EnumExample1/EnumExample1/Card.cs
@@ -4,6 +4,8 @@ public class Card
 {
     public CardSuit Suit { get; set; }
     public CardValue Value { get; set; }
+    
+    public string Name => $"{Value} {Suit}";
 
     public Card(CardSuit suit, CardValue value)
     {
