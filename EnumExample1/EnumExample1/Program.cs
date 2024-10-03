7e2a3d8f5e88c90086ad049dedfe8a363dc2fadd
Patryk <patryk.rakowski@cwmlegal.pl>
2024-10-03 20:59:02 +0200

    Replace greeting with card generation and display logic.

--- EnumExample1/EnumExample1/Program.cs
+++ EnumExample1/EnumExample1/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Random random = new();
+        var suitNumber = random.Next(0, 4);
+        var valueNumber = random.Next(1, 14);
+        Card myCard = new Card((CardSuit)suitNumber, (CardValue)valueNumber);
+        Console.WriteLine(myCard.Name);
     }
 }
