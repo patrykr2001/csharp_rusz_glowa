fe1b1a5ac52af35afaf677292ba4d5a07badae9c
Patryk <patryk.rakowski@cwmlegal.pl>
2024-10-07 23:19:08 +0200

    Add favorite foods lookup functionality to the console application.

--- dictionary_example/dictionary_example/Program.cs
+++ dictionary_example/dictionary_example/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Dictionary<string, string> favoriteFoods = new Dictionary<string, string>();
+        favoriteFoods["Alek"] = "hot dog";
+        favoriteFoods["Asia"] = "pizza";
+        favoriteFoods["Julek"] = "falafel";
+        favoriteFoods["Natalka"] = "spaghetti";
+
+        string name;
+        while ((name = Console.ReadLine()) != "")
+        {
+            if (favoriteFoods.ContainsKey(name))
+            {
+                Console.WriteLine($"Tym co {name} lubi najbardziej, jest {favoriteFoods[name]}");
+            }
+            else
+            {
+                Console.WriteLine($"Nie wiem, co lubi {name}.");
+            }
+        }
     }
 }
