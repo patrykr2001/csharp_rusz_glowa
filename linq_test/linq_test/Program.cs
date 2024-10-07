dd4fe6378176622b7f5296eb5c6ee6ad7c96da8d
Patryk <patryk.rakowski@cwmlegal.pl>
2024-10-08 00:27:47 +0200

    Add LINQ query to filter and order an array of integers.

--- linq_test/linq_test/Program.cs
+++ linq_test/linq_test/Program.cs
@@ -12,5 +12,17 @@ class Program
         IEnumerable<int> firstAndLastFive = numbers.Take(5).Concat(numbers.TakeLast(5));
         foreach(var i in firstAndLastFive)
             Console.Write($"{i} ");
+        Console.WriteLine();
+        
+        int[] values = new int[] { 0, 12, 44, 36, 92, 54, 13, 8 };
+
+        IEnumerable<int> result = 
+            from v in values 
+            where v < 37 
+            orderby -v 
+            select v;
+        foreach(var i in result)
+            Console.Write($"{i} ");
+        Console.WriteLine();
     }
 }
