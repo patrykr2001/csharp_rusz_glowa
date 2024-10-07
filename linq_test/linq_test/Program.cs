aa6816a8b49523359aa209636cfbc97a89e87b5f
Patryk <patryk.rakowski@cwmlegal.pl>
2024-10-08 00:21:21 +0200

    Add project and solution files with a basic program implementation.

---
+++ linq_test/linq_test/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace linq_test;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> numbers = new List<int>();
+        for (int i = 0; i <= 99; i++)
+        {
+            numbers.Add(i);
+        }
+        IEnumerable<int> firstAndLastFive = numbers.Take(5).Concat(numbers.TakeLast(5));
+        foreach(var i in firstAndLastFive)
+            Console.Write($"{i} ");
+    }
+}
