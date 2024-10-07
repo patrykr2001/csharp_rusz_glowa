9fd1cb413f2d88b8685eb0cdeab7fb355967fad8
Patryk <patryk.rakowski@cwmlegal.pl>
2024-10-08 00:37:00 +0200

    Add comic catalog and pricing information to the Comic class.

--- jacks_comics/jacks_comics/Comic.cs
+++ jacks_comics/jacks_comics/Comic.cs
@@ -6,4 +6,28 @@ public class Comic
     public int Issue { get; set; }
     
     public override string ToString() => $"{Name} (Numer {Issue})";
+
+    public static readonly IEnumerable<Comic> Catalog = new[]
+    {
+        new Comic() { Name = "Johny America vs. The Pinko", Issue = 6 },
+        new Comic() { Name = "Rock and Roll (edycja limitowana)", Issue = 19 },
+        new Comic() { Name = "Woman's Work", Issue = 36 },
+        new Comic() { Name = "Hippie Madness (z błędami w druku)", Issue = 57 },
+        new Comic() { Name = "Revenge of the New Wave Freak (uszkodzony)", Issue = 68 },
+        new Comic() { Name = "Black Monday", Issue = 74 },
+        new Comic() { Name = "Tribal Tattoo Madness", Issue = 83 },
+        new Comic() { Name = "Thhe Death of the Object ", Issue = 97 },
+    };
+
+    public static readonly Dictionary<int, decimal> Prices = new Dictionary<int, decimal>()
+    {
+        { 6, 3600m },
+        { 19, 500m },
+        { 36, 650m },
+        { 57, 13525m },
+        { 68, 250m },
+        { 74, 75m },
+        { 83, 25.75m },
+        { 87, 35.25m },
+    };
 }
