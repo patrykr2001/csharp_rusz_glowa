850f578e999b25ca20ef5f7377281ec46c0c3321
Patryk <patryk.rakowski@cwmlegal.pl>
2024-10-07 23:31:10 +0200

    Replace "Hello, World!" with a dictionary of retired Yankees players.

--- yankees_dictionary_example/yankees_dictionary_example/Program.cs
+++ yankees_dictionary_example/yankees_dictionary_example/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Dictionary<int, RetiredPlayer> retiredYankees = new Dictionary<int, RetiredPlayer>()
+        {
+            {3, new RetiredPlayer("Babe Ruth", 1948)},
+            {4, new RetiredPlayer("Lou Gehrig", 1939)},
+            {5, new RetiredPlayer("Joe DiMaggio", 1952)},
+            {7, new RetiredPlayer("Mickey Mantle", 1969)},
+            {8, new RetiredPlayer("Yogi Berra", 1972)},
+            {10, new RetiredPlayer("Phil Rizzuto", 1985)},
+            {23, new RetiredPlayer("Don Mattingly", 1997)},
+            {42, new RetiredPlayer("Jacke Robinson", 1993)},
+            {44, new RetiredPlayer("Reggie Jackson", 1993)},
+        };
+        foreach (int jerseyNumber in retiredYankees.Keys)
+        {
+            var player = retiredYankees[jerseyNumber];
+            Console.WriteLine($"{player.Name}, nr {jerseyNumber}, zakończył karierę w roku {player.YearRetired}.");
+        }
     }
 }
