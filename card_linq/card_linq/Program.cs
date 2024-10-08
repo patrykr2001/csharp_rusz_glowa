8d1b20454a248a1042cb750fad25c6328340675c
Patryk <patryk.rakowski@cwmlegal.pl>
2024-10-08 16:10:03 +0200

    Refactor Main method to display grouped card information from deck.

--- card_linq/card_linq/Program.cs
+++ card_linq/card_linq/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var deck = 
+            new Deck()
+            .Shuffle()
+            .Take(16);
+        
+        var grouped = 
+            from card in deck
+            group card by card.Suit into suitGroup
+            orderby suitGroup.Key descending
+            select suitGroup;
+
+        foreach (var group in grouped)
+        {
+            Console.WriteLine(@$"Grupa: {group.Key}
+Liczba elementów: {group.Count()}
+Minimum: {group.Min()}
+Maximum: {group.Max()}");
+        }
     }
 }
