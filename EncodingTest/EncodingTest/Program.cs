1a3735c1cfb0f5b3f9834b6da868d2f14ea521f4
Patryk Rakowski <patryk.rakowski@epoczta.pl>
2024-11-23 00:50:23 +0100

    Add initial project files for EncodingTest including main program logic.

---
+++ EncodingTest/EncodingTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+class Program
+{
+    static void Main()
+    {
+        using var ms = new MemoryStream();
+        using (var sw = new StreamWriter(ms))
+        {
+            sw.WriteLine("Wartość wynosi {0:0.00}.", 123.45678);
+            sw.Flush();
+            Console.WriteLine(Encoding.UTF8.GetString(ms.ToArray()));
+        }
+    }
+}
