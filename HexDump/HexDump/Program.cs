8a3a343cf4511bd795b394cf2b1e1281826dc109
Patryk Rakowski <95425713+patrykr2001@users.noreply.github.com>
2024-12-05 01:14:35 +0100

    Completed implementation

--- HexDump/HexDump/Program.cs
+++ HexDump/HexDump/Program.cs
@@ -4,6 +4,52 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        var position = 0;
+        using (var reader = new StreamReader("C:\\Users\\patry\\Learning\\C#\\csharp_rusz_glowa\\HexDump\\HexDump\\danetekstowe.txt"))
+        {
+            while (!reader.EndOfStream)
+            {
+                var buffer = new char[16];
+                var bytesRead = reader.ReadBlock(buffer, 0, buffer.Length);
+                if (bytesRead == 0)
+                {
+                    break;
+                }
+
+                Console.Write($"{position:X4}  ");
+                position += bytesRead;
+
+                for (var i = 0; i < 16; i++)
+                {
+                    if (i < bytesRead)
+                    {
+                        Console.Write($"{(byte)buffer[i]:X2} ");
+                    }
+                    else
+                    {
+                        Console.Write("   ");
+                    }
+
+                    if (i == 7)
+                    {
+                        Console.Write(" ");
+                    }
+                }
+
+                Console.Write(" ");
+                for (var i = 0; i < bytesRead; i++)
+                {
+                    var ch = (char)buffer[i];
+                    if (char.IsWhiteSpace(ch) || char.IsControl(ch))
+                    {
+                        ch = '.';
+                    }
+
+                    Console.Write(ch);
+                }
+
+                Console.WriteLine();
+            }
+        }
     }
 }
