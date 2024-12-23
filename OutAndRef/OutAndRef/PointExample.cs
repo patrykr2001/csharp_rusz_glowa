6d89edbdff87a3cfd6e9d4c42fbf2456f0004657
Patryk Rakowski <95425713+patrykr2001@users.noreply.github.com>
2024-12-23 13:46:13 +0100

    Add PointExample class with Point structure and cloning method.

---
+++ OutAndRef/OutAndRef/PointExample.cs
@@ -0,0 +1,23 @@
+﻿namespace OutAndRef;
+
+public class PointExample
+{
+    public class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public override string ToString()
+        {
+            return $"X: {X}, Y: {Y}";
+        }
+
+        public Point Clone()
+        {
+            return new Point { X = this.X, Y = this.Y };
+        }
+    }
+
+    private Point location;
+
+    public Point Location => location.Clone();
+}
